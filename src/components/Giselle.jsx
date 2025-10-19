0c5c9da98c9d68803991803e7afe9f397d94d1e4
jackie <jackie123768@gmail.com>
2025-10-19 12:39:11 -0700

    add project

---
+++ src/components/Giselle.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function Giselle() {
+
+  return (
+    <>
+      <h1>Hi Giselle</h1>
+    </>
+  )
+}
+
+export default Giselle
---
+++ src/components/Keryssa.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function Keryssa() {
+
+  return (
+    <>
+      <h1>Hi Keryssa</h1>
+    </>
+  )
+}
+
+export default Keryssa
